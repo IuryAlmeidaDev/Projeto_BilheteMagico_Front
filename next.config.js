1111b5c24b8594c3e9326af43928dea95945fd90
IuryAlmeidaDev <iury.colombo@gmail.com>
2026-04-07 11:51:07 -0400

    Add GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
-  outputFileTracingRoot: __dirname
+  output: 'export',
+  outputFileTracingRoot: __dirname,
+  basePath: isGitHubPages ? '/Projeto_BilheteMagico_Front' : '',
+  assetPrefix: isGitHubPages ? '/Projeto_BilheteMagico_Front/' : ''
 };
 
 module.exports = nextConfig;
